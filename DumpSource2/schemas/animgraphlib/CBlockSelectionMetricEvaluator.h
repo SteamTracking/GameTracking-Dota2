0a4bba3e1466aec8c542b4bb8e1dedcd0b217969
Wendy <freeman@steamdb.info>
2026-02-12 03:57:53 +0000

    6706 | 9 files | M DumpSource2/schemas/animgraphlib/CBlockSelectionMetricEvaluator.h,  M Protobufs/dota_gcmessages_server.proto,  M game/bin/linuxsteamrt64/libengine2_strings.txt,  M game/bin/linuxsteamrt64/libtier0_strings.txt,  M game/dota/bin/linuxsteamrt64/libclient_strings.txt,  M game/dota/bin/linuxsteamrt64/libserver_strings.txt,  M game/dota/pak01_dir.txt,  M game/dota/readonly_tools_asset_info.txt,  M game/dota/steam.inf
    https://steamdb.info/patchnotes/21893889/

--- DumpSource2/schemas/animgraphlib/CBlockSelectionMetricEvaluator.h
+++ DumpSource2/schemas/animgraphlib/CBlockSelectionMetricEvaluator.h
@@ -2,11 +2,11 @@
 //	"_class": "CBlockSelectionMetricEvaluator",
 //	"m_means":
 //	[
-//		-402975096832.000000
+//		-0.000000
 //	],
 //	"m_standardDeviations":
 //	[
-//		-402976145408.000000
+//		-0.000000
 //	],
 //	"m_flWeight": 0.000000,
 //	"m_nDimensionStartIndex": -1
